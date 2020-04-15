0b1ae73de581fa753b74d6d6878b1b1255927c5a
David Lutta <davidlutta2010@gmail.com>
2020-04-15 19:17:21 +0300

    First commit:

---
+++ app/src/main/java/com/davidlutta/filamu/models/Movies.java
@@ -0,0 +1,289 @@
+
+package com.davidlutta.filamu.models;
+
+import java.io.Serializable;
+import java.util.List;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.os.Parcelable.Creator;
+
+import androidx.annotation.NonNull;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Movies implements Serializable, Parcelable
+{
+
+    @SerializedName("popularity")
+    @Expose
+    private Double popularity;
+    @SerializedName("vote_count")
+    @Expose
+    private Long voteCount;
+    @SerializedName("video")
+    @Expose
+    private Boolean video;
+    @SerializedName("poster_path")
+    @Expose
+    private String posterPath;
+    @SerializedName("id")
+    @Expose
+    private Long id;
+    @SerializedName("adult")
+    @Expose
+    private Boolean adult;
+    @SerializedName("backdrop_path")
+    @Expose
+    private String backdropPath;
+    @SerializedName("original_language")
+    @Expose
+    private String originalLanguage;
+    @SerializedName("original_title")
+    @Expose
+    private String originalTitle;
+    @SerializedName("genre_ids")
+    @Expose
+    private List<Long> genreIds = null;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("vote_average")
+    @Expose
+    private Double voteAverage;
+    @SerializedName("overview")
+    @Expose
+    private String overview;
+    @SerializedName("release_date")
+    @Expose
+    private String releaseDate;
+    public final static Creator<Movies> CREATOR = new Creator<Movies>() {
+
+
+        @SuppressWarnings({
+            "unchecked"
+        })
+        public Movies createFromParcel(Parcel in) {
+            return new Movies(in);
+        }
+
+        public Movies[] newArray(int size) {
+            return (new Movies[size]);
+        }
+
+    }
+    ;
+    private final static long serialVersionUID = 1121456109942826512L;
+
+    protected Movies(Parcel in) {
+        this.popularity = ((Double) in.readValue((Double.class.getClassLoader())));
+        this.voteCount = ((Long) in.readValue((Long.class.getClassLoader())));
+        this.video = ((Boolean) in.readValue((Boolean.class.getClassLoader())));
+        this.posterPath = ((String) in.readValue((String.class.getClassLoader())));
+        this.id = ((Long) in.readValue((Long.class.getClassLoader())));
+        this.adult = ((Boolean) in.readValue((Boolean.class.getClassLoader())));
+        this.backdropPath = ((String) in.readValue((String.class.getClassLoader())));
+        this.originalLanguage = ((String) in.readValue((String.class.getClassLoader())));
+        this.originalTitle = ((String) in.readValue((String.class.getClassLoader())));
+        in.readList(this.genreIds, (Long.class.getClassLoader()));
+        this.title = ((String) in.readValue((String.class.getClassLoader())));
+        this.voteAverage = ((Double) in.readValue((Double.class.getClassLoader())));
+        this.overview = ((String) in.readValue((String.class.getClassLoader())));
+        this.releaseDate = ((String) in.readValue((String.class.getClassLoader())));
+    }
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public Movies() {
+    }
+
+    /**
+     * 
+     * @param overview
+     * @param voteAverage
+     * @param releaseDate
+     * @param video
+     * @param originalLanguage
+     * @param genreIds
+     * @param title
+     * @param originalTitle
+     * @param popularity
+     * @param voteCount
+     * @param id
+     * @param backdropPath
+     * @param adult
+     * @param posterPath
+     */
+    public Movies(Double popularity, Long voteCount, Boolean video, String posterPath, Long id, Boolean adult, String backdropPath, String originalLanguage, String originalTitle, List<Long> genreIds, String title, Double voteAverage, String overview, String releaseDate) {
+        super();
+        this.popularity = popularity;
+        this.voteCount = voteCount;
+        this.video = video;
+        this.posterPath = posterPath;
+        this.id = id;
+        this.adult = adult;
+        this.backdropPath = backdropPath;
+        this.originalLanguage = originalLanguage;
+        this.originalTitle = originalTitle;
+        this.genreIds = genreIds;
+        this.title = title;
+        this.voteAverage = voteAverage;
+        this.overview = overview;
+        this.releaseDate = releaseDate;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public Long getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Long voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public List<Long> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Long> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Movies{" +
+                "popularity=" + popularity +
+                ", voteCount=" + voteCount +
+                ", video=" + video +
+                ", posterPath='" + posterPath + '\'' +
+                ", id=" + id +
+                ", adult=" + adult +
+                ", backdropPath='" + backdropPath + '\'' +
+                ", originalLanguage='" + originalLanguage + '\'' +
+                ", originalTitle='" + originalTitle + '\'' +
+                ", title='" + title + '\'' +
+                ", voteAverage=" + voteAverage +
+                ", overview='" + overview + '\'' +
+                ", releaseDate='" + releaseDate + '\'' +
+                '}';
+    }
+
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(popularity);
+        dest.writeValue(voteCount);
+        dest.writeValue(video);
+        dest.writeValue(posterPath);
+        dest.writeValue(id);
+        dest.writeValue(adult);
+        dest.writeValue(backdropPath);
+        dest.writeValue(originalLanguage);
+        dest.writeValue(originalTitle);
+        dest.writeList(genreIds);
+        dest.writeValue(title);
+        dest.writeValue(voteAverage);
+        dest.writeValue(overview);
+        dest.writeValue(releaseDate);
+    }
+
+    public int describeContents() {
+        return  0;
+    }
+
+}
