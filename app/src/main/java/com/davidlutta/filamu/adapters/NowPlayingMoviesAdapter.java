0bb35aff92a4dcd17e937e512dc1ed305268668a
David Lutta <davidlutta2010@gmail.com>
2020-04-16 18:48:49 +0300

    Remove uneccessary files

--- app/src/main/java/com/davidlutta/filamu/adapters/NowPlayingMoviesAdapter.java
+++
@@ -1,5 +0,0 @@
-package com.davidlutta.filamu.adapters;
-
-public class NowPlayingMoviesAdapter {
-    // TODO: 4/16/20 CREATE VIEW HOLDER AND OTHER STUFF
-}
--- app/src/main/java/com/davidlutta/filamu/adapters/OnMovieListener.java
+++
@@ -1,5 +0,0 @@
-package com.davidlutta.filamu.adapters;
-
-public interface OnMovieListener {
-    void OnMovieClick(int position);
-}
