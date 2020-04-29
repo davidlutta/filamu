1909ca4460eb564c661492c25b0dc4568b7ca28a
David Lutta <davidlutta2010@gmail.com>
2020-04-30 02:57:00 +0300

    Add datasource, datasourceFactory and view model to view all series on air

--- app/src/main/java/com/davidlutta/filamu/UI/series/seriesOnAir/ViewAllSeriesOnAirFragment.java
+++ app/src/main/java/com/davidlutta/filamu/UI/series/seriesOnAir/ViewAllSeriesOnAirFragment.java
@@ -2,6 +2,7 @@ package com.davidlutta.filamu.UI.series.seriesOnAir;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -9,6 +10,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,12 +21,17 @@ import android.view.ViewGroup;
 
 import com.davidlutta.filamu.R;
 import com.davidlutta.filamu.UI.series.seriesOnAir.viewmodel.ViewAllSeriesOnAirViewModel;
+import com.davidlutta.filamu.adapters.series.ViewAllSeriesAdapter;
+import com.davidlutta.filamu.models.series.Series;
 
 import java.util.Objects;
 
-public class ViewAllSeriesOnAirFragment extends Fragment {
+public class ViewAllSeriesOnAirFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private ViewAllSeriesOnAirViewModel mViewModel;
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private RecyclerView recyclerView;
+    private ViewAllSeriesAdapter seriesAdapter;
 
     public static ViewAllSeriesOnAirFragment newInstance() {
         return new ViewAllSeriesOnAirFragment();
@@ -35,6 +45,8 @@ public class ViewAllSeriesOnAirFragment extends Fragment {
         toolbar.setTitle("Series On Air");
         ((AppCompatActivity) Objects.requireNonNull(getActivity())).setSupportActionBar(toolbar);
         ((AppCompatActivity) Objects.requireNonNull(getActivity())).setTitle("Series On Air");
+        recyclerView = view.findViewById(R.id.viewAllOnAirRecyclerView);
+        swipeRefreshLayout = view.findViewById(R.id.onAirSwipeRefreshLayout);
         return view;
     }
 
@@ -43,6 +55,31 @@ public class ViewAllSeriesOnAirFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(ViewAllSeriesOnAirViewModel.class);
         // TODO: Use the ViewModel
+        subscribeViewModels();
+        setUpAdapter();
     }
 
+    private void subscribeViewModels() {
+        mViewModel.getPagedListLiveData().observe(getViewLifecycleOwner(), new Observer<PagedList<Series>>() {
+            @Override
+            public void onChanged(PagedList<Series> series) {
+                swipeRefreshLayout.setRefreshing(false);
+                seriesAdapter.submitList(series);
+            }
+        });
+    }
+
+    private void setUpAdapter() {
+        if (seriesAdapter == null) {
+            seriesAdapter = new ViewAllSeriesAdapter(getContext());
+            RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getContext(), 2);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(seriesAdapter);
+        }
+    }
+
+    @Override
+    public void onRefresh() {
+        subscribeViewModels();
+    }
 }
--- app/src/main/java/com/davidlutta/filamu/UI/series/seriesOnAir/viewmodel/ViewAllSeriesOnAirViewModel.java
+++ app/src/main/java/com/davidlutta/filamu/UI/series/seriesOnAir/viewmodel/ViewAllSeriesOnAirViewModel.java
@@ -1,7 +1,48 @@
 package com.davidlutta.filamu.UI.series.seriesOnAir.viewmodel;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class ViewAllSeriesOnAirViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+
+import com.davidlutta.filamu.UI.series.seriesOnAir.datasource.SeriesDataSource;
+import com.davidlutta.filamu.UI.series.seriesOnAir.datasource.SeriesDataSourceFactory;
+import com.davidlutta.filamu.models.series.Series;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class ViewAllSeriesOnAirViewModel extends AndroidViewModel {
+    private SeriesDataSourceFactory seriesDataSourceFactory;
+    private MutableLiveData<SeriesDataSource> seriesDataSourceMutableLiveData;
+    private LiveData<PagedList<Series>> pagedListLiveData;
+    private Executor executor;
+
+    public ViewAllSeriesOnAirViewModel(@NonNull Application application) {
+        super(application);
+        seriesDataSourceFactory = new SeriesDataSourceFactory();
+        seriesDataSourceMutableLiveData = seriesDataSourceFactory.getMutableLiveData();
+        PagedList.Config config = (new PagedList.Config.Builder())
+                .setEnablePlaceholders(true)
+                .setInitialLoadSizeHint(10)
+                .setPageSize(20)
+                .setPrefetchDistance(4)
+                .build();
+        executor = Executors.newFixedThreadPool(5);
+        pagedListLiveData = new LivePagedListBuilder<Integer, Series>(seriesDataSourceFactory, config)
+                .setFetchExecutor(executor)
+                .build();
+    }
+
+    public LiveData<PagedList<Series>> getPagedListLiveData() {
+        if (pagedListLiveData != null) {
+            return pagedListLiveData;
+        }else
+            return null;
+    }
 }
